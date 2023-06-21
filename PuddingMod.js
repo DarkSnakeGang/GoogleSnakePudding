f7cd4bb2aae67fd317090fe471e17c15894a1e31
Benny Yarmolovich <someonesomewheremax@gmail.com>
2023-06-21 16:42:36 +0300

    Debug dynamic

--- PuddingMod.js
+++ PuddingMod.js
@@ -27,7 +27,12 @@ window.PuddingMod.runCodeBefore = function () {
     req.send();
   }
 
+
   window.NepDebug = false;
+  if (localStorage.getItem('snakeChosenMod') === "customUrl") {
+    console.log("Detect customUrl - enabling debug mode")
+    window.NepDebug = true;
+  }
 
   window.Libraries = ["Core", "Theme", "DistinctVisual", "Counter", "TimeKeeper", "Fruit", "TopBar", "SnakeColor", "BootstrapMenu"];
   console.log("Enabling Pudding Mod");
