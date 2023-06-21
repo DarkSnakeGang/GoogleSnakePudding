a004b236fbd81cc75e8a55b66a80bf7a26d11563
Cytruss-fruit <iwillflagya@gmail.com>
2023-06-21 20:14:17 +0300

    Display works on google version

--- Libraries/InputDisplay.js
+++ Libraries/InputDisplay.js
@@ -6,34 +6,34 @@ window.InputDisplay.make = function () {
     // Recommended to use "window." for things
     const e = document.createElement('div');
   e.id = 'input-display-container';
-  e.style = 'position:absolute;left:156px;top:516px;z-index:10000;display:block';
+  e.style = 'position:absolute;left:156px;top:516px;z-index:10000;display:block;line-height:normal;';
   document.getElementsByClassName('sEOCsb')[0].appendChild(e);
 
   const f = document.createElement('div');
   f.id = 'input-display-container';
-  f.style = 'position:absolute;left:156px;top:444px;z-index:10000;display:block';
+  f.style = 'position:absolute;left:156px;top:444px;z-index:10000;display:block;line-height:normal;';
   document.getElementsByClassName('sEOCsb')[0].appendChild(f);
 
   const InpBox = document.querySelector('#input-display-container');
 
   const LeftButton = document.createElement('div');
   LeftButton.style ='position:absolute;left:460px;top"450px;z-index:10000;width:200px;';
-  LeftButton.innerHTML = '<div id="left-button-id" style="font-size:40px;color:white;display:none;background-color:#4f4f4f;font-family:Roboto,Arial,sans-serif;vertical-align:middle;padding-bottom:12px;padding-right:10px;padding-left:10px;">←</div>'
+  LeftButton.innerHTML = '<div id="left-button-id" style="font-size:40px;color:white;display:none;line-height:normal;background-color:#4f4f4f;font-family:Roboto,Arial,sans-serif;vertical-align:middle;padding-bottom:12px;padding-right:10px;padding-left:10px;">←</div>'
   InpBox.appendChild(LeftButton);
 
   const DownButton = document.createElement('div');
   DownButton.style ='position:absolute;left:530px;top"452px;z-index:10000;width:200px;';
-  DownButton.innerHTML = '<div id="down-button-id" style="font-size:40px;color:white;display:none;background-color:#4f4f4f;font-family:Roboto,Arial,sans-serif;vertical-align:middle;padding-bottom:10px;padding-top:2px;padding-right:21px;padding-left:21px;">  ↓  </div>'
+  DownButton.innerHTML = '<div id="down-button-id" style="font-size:40px;color:white;display:none;line-height:normal;background-color:#4f4f4f;font-family:Roboto,Arial,sans-serif;vertical-align:middle;padding-bottom:10px;padding-top:2px;padding-right:21px;padding-left:21px;">  ↓  </div>'
   InpBox.appendChild(DownButton);
 
   const RightButton = document.createElement('div');
   RightButton.style ='position:absolute;left:601px;top"550px;z-index:10000;width:200px;';
-  RightButton.innerHTML = '<div id="right-button-id" style="font-size:40px;color:white;display:none;background-color:#4f4f4f;font-family:Roboto,Arial,sans-serif;vertical-align:middle;padding-bottom:12px;padding-right:10px;padding-left:10px;">→</div>'
+  RightButton.innerHTML = '<div id="right-button-id" style="font-size:40px;color:white;display:none;line-height:normal;background-color:#4f4f4f;font-family:Roboto,Arial,sans-serif;vertical-align:middle;padding-bottom:12px;padding-right:10px;padding-left:10px;">→</div>'
   InpBox.appendChild(RightButton);
 
   const TopButton = document.createElement('div');
   TopButton.style ='position:relative;left:530px;top"152px;z-index:10000;width:200px;';
-  TopButton.innerHTML = '<div id="top-button-id" style="font-size:40px;color:white;display:none;background-color:#4f4f4f;font-family:Roboto,Arial,sans-serif;vertical-align:middle;padding-bottom:10px;padding-top:2px;padding-right:21px;padding-left:21px;">↑</div>'
+  TopButton.innerHTML = '<div id="top-button-id" style="font-size:40px;color:white;display:none;line-height:normal;background-color:#4f4f4f;font-family:Roboto,Arial,sans-serif;vertical-align:middle;padding-bottom:10px;padding-top:2px;padding-right:21px;padding-left:21px;">↑</div>'
   f.appendChild(TopButton);
 
   window.input_toggle =false;
