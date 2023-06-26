69db1e5689c2184aa25340d0d446e1d0cb7e8c19
Benny Yarmolovich <someonesomewheremax@gmail.com>
2023-06-27 01:05:27 +0300

    Random theme stuff

--- Libraries/Theme.js
+++ Libraries/Theme.js
@@ -2,6 +2,277 @@ window.Theme = {};
 
 window.Theme.make = function () {
 
+  let advancedSettings = JSON.parse(localStorage.getItem('snakeAdvancedSettings')) ?? {};
+
+  window.themes = [
+    {
+      name: 'Default Sun',
+      light_tiles: '#aad751',
+      dark_tiles: '#a2d149',
+      shadow: '#94bd46',
+      border: '#578a34',
+      key_block_sign_color: '#38640e',
+      real_top_bar: '#4a752c',
+      endscreen_background: '#4dc1f9',
+      sep_color: '#7eccfa',
+      topbar_color: '#3a91bb',
+      buttons_color: '#1155CC',
+      bg_color: '#4dc1f9',
+      bottom_color: '#4dc1f9'
+    },
+    {
+      name: 'Official Dark',
+      light_tiles: '#494351',
+      dark_tiles: '#443e4c',
+      shadow: '#3d3644',
+      border: '#2c2730',
+      key_block_sign_color: '#453d4d',
+      real_top_bar: '#262428',
+      endscreen_background: '#2a2640',
+      sep_color: '#363438',
+      topbar_color: '#111111',
+      buttons_color: '#111111',
+      bg_color: '#262428',
+      bottom_color: '#262428'
+    },
+    {
+      name: 'Snow',
+      light_tiles: '#deeced',
+      dark_tiles: '#d1e4e6',
+      shadow: '#b9d4d5',
+      border: '#879fa1',
+      key_block_sign_color: '#506486',
+      real_top_bar: '#75898a',
+      endscreen_background: '#8cbfd9',
+      sep_color: '#85999a',
+      topbar_color: '#677f91',
+      buttons_color: '#677f91',
+      bg_color: '#75898a',
+      bottom_color: '#75898a'
+    },
+    {
+      name: 'Volcano',
+      light_tiles: '#6e3535',
+      dark_tiles: '#673232',
+      shadow: '#633131',
+      border: '#a33e3e',
+      key_block_sign_color: '#642b2b',
+      real_top_bar: '#762d2d',
+      endscreen_background: '#292e4c',
+      sep_color: '#863d3d',
+      topbar_color: '#a33e3e',
+      buttons_color: '#a33e3e',
+      bg_color: '#762d2d',
+      bottom_color: '#762d2d'
+    },
+    {
+      name: 'Desert',
+      light_tiles: '#f2d78c',
+      dark_tiles: '#eccd79',
+      shadow: '#e6c770',
+      border: '#977b26',
+      key_block_sign_color: '#594d26',
+      real_top_bar: '#725e1d',
+      endscreen_background: '#5fb7e3',
+      sep_color: '#826e2d',
+      topbar_color: '#977b26',
+      buttons_color: '#977b26',
+      bg_color: '#725e1d',
+      bottom_color: '#725e1d'
+    },
+    {
+      name: 'Official Jungle',
+      light_tiles: '#3f5543',
+      dark_tiles: '#3b4f3f',
+      shadow: '#334737',
+      border: '#253227',
+      key_block_sign_color: '#354b38',
+      real_top_bar: '#202822',
+      endscreen_background: '#2b375a',
+      sep_color: '#303832',
+      topbar_color: '#253227',
+      buttons_color: '#253227',
+      bg_color: '#202822',
+      bottom_color: '#202822'
+    },
+    {
+      name: 'Pool',
+      light_tiles: '#b4d0f9',
+      dark_tiles: '#a3c5f5',
+      shadow: '#94baf0',
+      border: '#275ba5',
+      key_block_sign_color: '#11325f',
+      real_top_bar: '#1d457c',
+      endscreen_background: '#42a5f0',
+      sep_color: '#2d558c',
+      topbar_color: '#275ba5',
+      buttons_color: '#1155CC',
+      bg_color: '#1d457c',
+      bottom_color: '#1d457c'
+    },
+    {
+      name: 'Space',
+      light_tiles: '#432c68',
+      dark_tiles: '#3d285d',
+      shadow: '#3a2956',
+      border: '#604096',
+      key_block_sign_color: '#3f305a',
+      real_top_bar: '#432a6f',
+      endscreen_background: '#32224f',
+      sep_color: '#533a7f',
+      topbar_color: '#604096',
+      buttons_color: '#604096',
+      bg_color: '#432a6f',
+      bottom_color: '#432a6f'
+    },
+    {
+      name: "Globe",
+      sep_color: '#7eccfa',
+      topbar_color: '#3a91bb',
+      real_top_bar: '#4a752c',
+      buttons_color: '#1155CC',
+      bg_color: '#4dc1f9',
+      bottom_color: '#4dc1f9'
+    },
+    {
+      name: 'True Dark',
+      light_tiles: '#1D1D1D',
+      dark_tiles: '#161616',
+      shadow: '#111111',
+      border: '#000000',
+      key_block_sign_color: '#1D1D1D',
+      real_top_bar: '#111111',
+      endscreen_background: '#000000',
+      sep_color: '#212121',
+      topbar_color: '#000000',
+      buttons_color: '#000000',
+      bg_color: '#111111',
+      bottom_color: '#111111'
+    },
+    {
+      name: 'Planeptune',
+      light_tiles: '#d0b4f9',
+      dark_tiles: '#c5a3f5',
+      shadow: '#ba94f0',
+      border: '#5b27a5',
+      key_block_sign_color: '#32115f',
+      real_top_bar: '#451d7c',
+      endscreen_background: '#a542f0',
+      sep_color: '#6b37b5',
+      topbar_color: '#5b27a5',
+      buttons_color: '#5b27a5',
+      bg_color: '#a542f0',
+      bottom_color: '#a542f0'
+    },
+    {
+      name: 'Lastation',
+      light_tiles: '#0050b0',
+      dark_tiles: '#0059b9',
+      shadow: '#003478',
+      border: '#000c30',
+      key_block_sign_color: '#0050b0',
+      real_top_bar: '#000220',
+      endscreen_background: '#000C30',
+      sep_color: '#101230',
+      topbar_color: '#01055C',
+      buttons_color: '#01055C',
+      bg_color: '#000c30',
+      bottom_color: '#000c30'
+    },
+    {
+      name: 'Pacman',
+      light_tiles: '#010101',
+      dark_tiles: '#000000',
+      shadow: '#000000',
+      border: '#0805c6',
+      key_block_sign_color: '#0000d2',
+      real_top_bar: '#0805c6',
+      endscreen_background: '#000000',
+      sep_color: '#000000',
+      topbar_color: '#0805c6',
+      buttons_color: '#0805c6',
+      bg_color: '#000000',
+      bottom_color: '#000000'
+    },
+    {
+      name: 'Sonic',
+      light_tiles: '#B25900',
+      dark_tiles: '#A05000',
+      shadow: '#333333',
+      border: '#124f00',
+      key_block_sign_color: '#0f81d8',
+      real_top_bar: '#2bb800',
+      endscreen_background: '#0f81d8',
+      sep_color: '#1f91e8',
+      topbar_color: '#124f00',
+      buttons_color: '#124f00',
+      bg_color: '#0f81d8',
+      bottom_color: '#0f81d8'
+    },
+    {
+      name: 'Jungle',
+      light_tiles: '#499D43',
+      dark_tiles: '#36982F',
+      shadow: '#336E2B',
+      border: '#335B36',
+      key_block_sign_color: '#ffef4f',
+      real_top_bar: '#476C42',
+      endscreen_background: '#13867E',
+      sep_color: '#47724C',
+      topbar_color: '#133B26',
+      buttons_color: '#133B26',
+      bg_color: '#37623C',
+      bottom_color: '#37623C'
+    },
+    {
+      name: 'Pudding',
+      light_tiles: '#ffef4f',
+      dark_tiles: '#ffdf3f',
+      shadow: '#dfbf1f',
+      border: '#a55229',
+      key_block_sign_color: '#eeeeee',
+      real_top_bar: '#853209',
+      endscreen_background: '#853209',
+      sep_color: '#efcf2f',
+      topbar_color: '#752209',
+      buttons_color: '#752209',
+      bg_color: '#dfbf1f',
+      bottom_color: '#dfbf1f'
+    },
+    {
+      name: 'Ice',
+      light_tiles: '#57DDFF',
+      dark_tiles: '#57D5F4',
+      shadow: '#57B0C7',
+      border: '#006080',
+      key_block_sign_color: '#005C8A',
+      real_top_bar: '#00495C',
+      endscreen_background: '#00E1E6',
+      sep_color: '#10C1C6',
+      topbar_color: '#00293C',
+      buttons_color: '#00293C',
+      bg_color: '#00B1B6',
+      bottom_color: '#00B1B6'
+    },
+    {
+      name: "ModLoader",
+      light_tiles: advancedSettings.themeCol1 ?? '#1D1D1D',
+      dark_tiles: advancedSettings.themeCol2 ?? '#161616',
+      shadow: advancedSettings.themeCol3 ?? '#111111',
+      border: advancedSettings.themeCol4 ?? '#000000',
+      key_block_sign_color: advancedSettings.themeCol5 ?? '#1D1D1D',
+      real_top_bar: advancedSettings.themeCol6 ?? '#111111',
+      endscreen_background: advancedSettings.themeCol7 ?? '#000000',
+      sep_color: '#7eccfa',
+      topbar_color: '#3a91bb',
+      real_top_bar: '#4a752c',
+      buttons_color: '#1155CC',
+      bg_color: '#4dc1f9',
+      bottom_color: '#4dc1f9'
+    }
+
+  ];
+
   for (let src of [
     'https://i.postimg.cc/R0R1ZMNx/dark-night.png',
     'https://i.postimg.cc/t4bxfYzt/planeptune.png',
@@ -12,6 +283,7 @@ window.Theme.make = function () {
     'https://i.postimg.cc/5y7gwwGY/pudding-cr.png',
     'https://i.postimg.cc/1XqLvbhJ/Ice2.png',
     'https://i.postimg.cc/HLr5YJmb/modloader-icon.png',
+    'https://i.postimg.cc/cCr9LrNZ/neptune-planet.png',
   ]) document.querySelector('#theme').appendChild(uiImage(src));
 
 }
@@ -49,26 +321,54 @@ window.Theme.alterCode = function (code) {
   window.real_topbar_color = "#4a752c";
   window.button_color = "#1155CC";
 
-  color_code = `
-    for(let p of window.ui_sep) {
-      let separators = sep_color;
-      p.style.borderBottomColor = separators;
+  // ChatGPT wrote this crap
+  function getAttributesByName(themeName) {
+    const theme = window.themes.find((theme) => theme.name === themeName);
+    if (theme) {
+      const { name, set_theme, ...attributes } = theme;
+      return attributes;
     }
-    for(let h of window.ui_topbar){
-      h.style.background = topbar_color;
+    return null; // Return null if theme doesn't exist
+  }
+
+  window.setTheme = function (theme_name) {
+
+    loop_array = [
+      { loop_on: window.ui_sep, attribute: "borderBottomColor", color: "sep_color" },
+      { loop_on: window.ui_topbar, attribute: "background", color: "topbar_color" },
+      { loop_on: window.ui_buttons, attribute: "background", color: "buttons_color" },
+      { loop_on: window.input_button, attribute: "background", color: "buttons_color" },
+      { loop_on: window.ui_background, attribute: "background", color: "bg_color" },
+      { loop_on: window.ui_bottom, attribute: "background", color: "bottom_color" },
+      { loop_on: window.boot_button, attribute: "backgroundColor", color: "buttons_color" },
+      { loop_on: window.boot_check, attribute: "backgroundColor", color: "buttons_color" },
+      { loop_on: window.boot_dropdown, attribute: "backgroundColor", color: "buttons_color" },
+    ]
+
+    const themeAttributes = getAttributesByName(theme_name);
+    if (themeAttributes) {
+      // Extract individual attribute values using destructuring
+      // ChatGPT wrote this crap
+      var {
+        light_tiles,
+        dark_tiles,
+        shadow,
+        border,
+        key_block_sign_color,
+        real_top_bar,
+        endscreen_background,
+        sep_color,
+        topbar_color,
+        buttons_color,
+        bg_color,
+        bottom_color,
+      } = themeAttributes;
     }
-    for(let h of window.ui_buttons){
-      h.style.background = buttons_color;
-    }
-    for(let h of window.input_button)
-    {
-      h.style.background = buttons_color;
-    }
-    for(let h of window.ui_background){
-      h.style.background = bg_color;
-    }
-    for(let h of window.ui_bottom){
-      h.style.background = bottom_color;
+
+    for (let element of loop_array) {
+      for (let h of element["loop_on"]) {
+        eval("h.style." + element["attribute"] + " = " + element["color"] + ";")
+      }
     }
 
     document.getElementById('settings-popup-pudding').style.background = real_top_bar;
@@ -76,244 +376,34 @@ window.Theme.alterCode = function (code) {
 
     window.real_topbar_color = real_top_bar;
     window.button_color = buttons_color;
-    for(let h of window.boot_button){
-      h.style.backgroundColor = buttons_color;
-    }
-    for(let h of window.boot_check){
-      h.style.backgroundColor = buttons_color;
-    }
-    for(let h of window.boot_dropdown){
-      h.style.backgroundColor = buttons_color;
-    }
 
-    if (set_theme) {
+
+    if (theme_name != "Globe") {
       window.snake.setCustomTheme(light_tiles, dark_tiles, shadow, border, key_block_sign_color, real_top_bar, endscreen_background)
     }
-    `
+    else {
+      window.snake.clearCustomTheme();
+    }
+  }
 
+  window.getRandomThemeName = function getRandomThemeName() {
+    const filteredThemes = window.themes.filter((theme) => theme.name !== 'Globe' && theme.name !== 'ModLoader');
+    const randomIndex = Math.floor(Math.random() * filteredThemes.length);
+    return filteredThemes[randomIndex].name;
+  }
 
+  window.randomTheme = false;
 
   code = code.assertReplace(/case "theme":/, `case "theme":
-  set_theme = true;
-  switch(d){
-    case 0:
-      set_theme = true;
-      light_tiles='#aad751';
-      dark_tiles='#a2d149';
-      shadow='#94bd46';
-      border='#578a34';
-      key_block_sign_color='#38640e';
-      real_top_bar='#4a752c';
-      endscreen_background='#4dc1f9'; // Default Sun
-      sep_color='#7eccfa';
-      topbar_color='#3a91bb';
-      buttons_color='#1155CC';
-      bg_color=bottom_color='#4dc1f9';
-    break;
-    case 1:
-      set_theme = true;
-      light_tiles='#494351';
-      dark_tiles='#443e4c';
-      shadow='#3d3644';
-      border='#2c2730';
-      key_block_sign_color='#453d4d';
-      real_top_bar='#262428';
-      endscreen_background='#2a2640'; // Official Dark
-      sep_color='#363438';
-      topbar_color=buttons_color='#111111';
-      bg_color=bottom_color='#262428';
-    break;
-    case 2: set_theme = true;
-      light_tiles='#deeced';
-      dark_tiles='#d1e4e6';
-      shadow='#b9d4d5';
-      border='#879fa1';
-      key_block_sign_color='#506486';
-      real_top_bar='#75898a';
-      endscreen_background='#8cbfd9'; // Snow
-      sep_color='#85999a';
-      topbar_color=buttons_color='#677f91';
-      bg_color=bottom_color='#75898a';
-    break;
-    case 3: set_theme = true;
-      light_tiles='#6e3535';
-      dark_tiles='#673232';
-      shadow='#633131';
-      border='#a33e3e';
-      key_block_sign_color='#642b2b';
-      real_top_bar='#762d2d';
-      endscreen_background='#292e4c'; // Volcano
-      sep_color='#863d3d';
-      topbar_color=buttons_color='#a33e3e';
-      bg_color=bottom_color='#762d2d';
-    break;
-    case 4: set_theme = true;
-      light_tiles='#f2d78c';
-      dark_tiles='#eccd79';
-      shadow='#e6c770';
-      border='#977b26';
-      key_block_sign_color='#594d26';
-      real_top_bar='#725e1d';
-      endscreen_background='#5fb7e3'; // Desert
-      sep_color='#826e2d';
-      topbar_color=buttons_color='#977b26';
-      bg_color=bottom_color='#725e1d';
-    break;
-    case 5: set_theme = true;
-      light_tiles='#3f5543';
-      dark_tiles='#3b4f3f';
-      shadow='#334737';
-      border='#253227';
-      key_block_sign_color='#354b38';
-      real_top_bar='#202822';
-      endscreen_background='#2b375a'; // Official Jungle
-      sep_color='#303832';
-      topbar_color=buttons_color='#253227';
-      bg_color=bottom_color='#202822';
-    break;
-    case 6: set_theme = true;
-      light_tiles='#b4d0f9';
-      dark_tiles='#a3c5f5';
-      shadow='#94baf0';
-      border='#275ba5';
-      key_block_sign_color='#11325f';
-      real_top_bar='#1d457c';
-      endscreen_background='#42a5f0'; // Pool
-      sep_color='#2d558c';
-      topbar_color='#275ba5';buttons_color='#1155CC';
-      bg_color=bottom_color='#1d457c';
-    break;
-    case 7: set_theme = true;
-      light_tiles='#432c68';
-      dark_tiles='#3d285d';
-      shadow='#3a2956';
-      border='#604096';
-      key_block_sign_color='#3f305a';
-      real_top_bar='#432a6f';
-      endscreen_background='#32224f'; // Space
-      sep_color='#533a7f';
-      topbar_color=buttons_color='#604096';
-      bg_color=bottom_color='#432a6f';
-    break;
-    case 8: set_theme = false;
-      window.snake.clearCustomTheme(); // Random Globe
-      sep_color='#7eccfa';
-      topbar_color='#3a91bb';
-      real_top_bar='#4a752c';
-      buttons_color='#1155CC';
-      bg_color=bottom_color='#4dc1f9';
-    break; // Randomize Globe Theme
-    case 9: set_theme = true;
-      light_tiles='#1D1D1D';
-      dark_tiles='#161616';
-      shadow='#111111';
-      border='#000000';
-      key_block_sign_color='#1D1D1D';
-      real_top_bar='#111111';
-      endscreen_background='#000000'; // True Dark
-      sep_color='#212121';
-      topbar_color=buttons_color='#000000';
-      bg_color=bottom_color='#111111';
-    break;
-    case 10: set_theme = true;
-      light_tiles='#d0b4f9';
-      dark_tiles='#c5a3f5';
-      shadow='#ba94f0';
-      border='#5b27a5';
-      key_block_sign_color='#32115f';
-      real_top_bar='#451d7c';
-      endscreen_background='#a542f0'; // Planeptune
-      sep_color='#6b37b5';
-      topbar_color=buttons_color='#5b27a5';
-      bg_color=bottom_color='#a542f0';
-    break;
-    case 11: set_theme = true;
-      light_tiles='#0050b0';
-      dark_tiles='#0059b9';
-      shadow='#003478';
-      border='#000c30';
-      key_block_sign_color='#0050b0';
-      real_top_bar='#000220';
-      endscreen_background='#000C30'; // Lastation
-      sep_color='#101230';
-      topbar_color=buttons_color='#01055C';
-      bg_color=bottom_color='#000c30';
-    break;
-    case 12: set_theme = true;
-      light_tiles='#010101';
-      dark_tiles='#000000';
-      shadow='#000000';
-      border='#0805c6';
-      key_block_sign_color='#000000';
-      real_top_bar='#000000';
-      endscreen_background='#000C30'; // Pacman
-      sep_color='#10104d';
-      topbar_color=buttons_color='#111111';
-      bg_color=bottom_color='#00003d';
-    break;
-    case 13: set_theme = true;
-      light_tiles='#B25900';
-      dark_tiles='#A05000';
-      shadow='#333333';
-      border='#124f00';
-      key_block_sign_color='#0f81d8';
-      real_top_bar='#2bb800';
-      endscreen_background='#0f81d8'; // Sonic
-      sep_color='#1f91e8';
-      topbar_color=buttons_color='#124f00';
-      bg_color=bottom_color='#0f81d8';
-    break;
-    case 14: set_theme = true;
-      light_tiles='#499D43';
-      dark_tiles='#36982F';
-      shadow='#336E2B';
-      border='#335B36';
-      key_block_sign_color='#ffef4f';
-      real_top_bar='#476C42';
-      endscreen_background='#13867E'; // Jungle
-      sep_color='#47724C';
-      topbar_color=buttons_color='#133B26';
-      bg_color=bottom_color='#37623C';
-    break;
-    case 15: set_theme = true;
-      light_tiles='#ffef4f';
-      dark_tiles='#ffdf3f';
-      shadow='#dfbf1f';
-      border='#a55229';
-      key_block_sign_color='#eeeeee';
-      real_top_bar='#853209';
-      endscreen_background='#853209'; // Pudding
-      sep_color='#efcf2f';
-      topbar_color=buttons_color='#752209';
-      bg_color=bottom_color='#dfbf1f';
-    break;
-    case 16: set_theme = true;
-      light_tiles='#57DDFF';
-      dark_tiles='#57D5F4';
-      shadow='#57B0C7';
-      border='#006080';
-      key_block_sign_color='#005C8A';
-      real_top_bar='#00495C';
-      endscreen_background='#00E1E6'; // Ice
-      sep_color='#10C1C6';
-      topbar_color=buttons_color='#00293C';
-      bg_color=bottom_color='#00B1B6';
-    break;
-    case 17: set_theme = true; let advancedSettings = JSON.parse(localStorage.getItem('snakeAdvancedSettings')) ?? {}; // ModLoader
-      light_tiles=advancedSettings.themeCol1 ?? '#1D1D1D';
-      dark_tiles=advancedSettings.themeCol2 ?? '#161616';
-      shadow=advancedSettings.themeCol3 ?? '#111111';
-      border=advancedSettings.themeCol4 ?? '#000000';
-      key_block_sign_color=advancedSettings.themeCol5 ?? '#1D1D1D';
-      real_top_bar=advancedSettings.themeCol6 ?? '#111111';
-      endscreen_background=advancedSettings.themeCol7 ?? '#000000';
-      sep_color='#7eccfa';
-      topbar_color='#3a91bb';
-      buttons_color='#1155CC';
-      bg_color=bottom_color='#4dc1f9';
-    break;
-  }
-  ${color_code}
+  if(d<window.themes.length){window.randomTheme = false;window.setTheme(window.themes[d].name);}
+  else{window.randomTheme = true;window.setTheme(window.getRandomThemeName());};
   `)
+
+  reset_regex = new RegExp(/;this\.reset\(\)/)
+
+  set_on_reset = `;
+  if(window.randomTheme){window.setTheme(window.getRandomThemeName());}
+  $&`
+  code = code.assertReplace(reset_regex, set_on_reset)
   return code;
 }
