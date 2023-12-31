cb4fc176077792463b93c6df3bd50d196d23c308
Benny Yarmolovich <6286286+Yarmiplay@users.noreply.github.com>
2023-12-31 14:29:01 +0200

    fixed UI shit

--- Libraries/BootstrapMenu.js
+++ Libraries/BootstrapMenu.js
@@ -135,7 +135,8 @@ window.BootstrapMenu.make = function () {
     <div class="form-check form-check-inline">
     <input class="form-check-input" type="checkbox" role="switch" id="AlwaysOnTimeKeeper">
     <label class="form-check-label" for="AlwaysOnTimeKeeper" style="margin:3px;color:white;font-family:Roboto,Arial,sans-serif;">Show SpeedInfo</label>
-
+    </div>
+    <div>
     <input class="form-check-input" type="checkbox" role="switch" id="DisableRandom">
     <label class="form-check-label" for="DisableRandom" style="margin:3px;color:white;font-family:Roboto,Arial,sans-serif;">Disable Randomizer</label>
     </div>
